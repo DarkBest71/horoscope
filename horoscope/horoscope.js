5983f283c7db0d85c8af011bccad7bb75893c134
seb03000 <avatardu33@gmail.com>
2017-10-20 17:16:05 +0200

    Update horoscope.js

--- horoscope/horoscope.js
+++ horoscope/horoscope.js
@@ -1,30 +1,40 @@
-exports.action = function(data, callback, config, SARAH){
+var ScribeAskMe; var ScribeSpeak; var maConfig; var SCRIBE;
+exports.action = function(data, callback, config,SARAH){
+SCRIBE = SARAH.context.scribe; ScribeAskMe = SARAH.ScribeAskMe; ScribeSpeak = SARAH.ScribeSpeak; SCRIBE.activePlugin('horoscope');
+phrasehoroscope=JSON.stringify(SARAH.context.scribe.lastReco);phrasehoroscope=JSON.parse(phrasehoroscope).toLowerCase()
 
- // CONFIG
-  config = config.modules.horoscope;
-  if (!config.signe){
-    console.log("horoscope config missing");
-    callback({ 'tts': "J'ai besoin de ton signe pour prédire l'avenir. Renseigne le dans le fichier de propriété" });
-    return;
-  }
-  
-  var signe = config.signe;
-  var url = 'http://www.horoscope-gratuit.org/horoscope-'+signe+'.php';
-  
-  var request = require('request');
-  request({ 'uri' : url }, function (err, response, body){
-    
-    if (err || response.statusCode != 200) {
-      callback({'tts': "Je n'arrive pas à prédire ton avenir"});
-      return;
-    }
-    var $ = require('cheerio').load(body, { xmlMode: true, ignoreWhitespace: false, lowerCaseTags: false });
-   	var horoscope = $('html.no-js body div.mn-doc div#ctn_global div#ctn_middle div#ctn_content_00.float_left div#ctn_horoscope_jour.ctn_horoscope p.ctn_categorie').text() ;
-	callback({ 'tts': horoscope});
-  
-  });
-else {
-	callback({'tts': txt});
-	}
-}
-exports.action = action;
+//phrasehoroscope=data.phrasehoroscope.toLowerCase();
+console.log('on a recu horoscope'+phrasehoroscope)
+config = config.modules.horoscopemathilde; signe = config.signe.toLowerCase();//default
+function signes(phrasehoroscope,signe){
+if (phrasehoroscope.search('cancer')>-1){signe='cancer'}
+if (phrasehoroscope.search('lion')>-1){signe='lion'}
+if (phrasehoroscope.search('scorpion')>-1){signe='scorpion'}
+if (phrasehoroscope.search('balance')>-1){signe='balance'}
+if (phrasehoroscope.search('poisson')>-1){signe='poissons'}
+if (phrasehoroscope.search('vierge')>-1){signe='vierge'}
+if (phrasehoroscope.search('bélier')>-1){signe='belier'}
+if (phrasehoroscope.search('taureau')>-1){signe='taureau'}
+if (phrasehoroscope.search('gémeaux')>-1){signe='gemeaux'}
+if (phrasehoroscope.search('sagittaire')>-1){signe='sagittaire'}
+if (phrasehoroscope.search('capricorne')>-1){signe='capricorne'}
+if (phrasehoroscope.search('verseau')>-1){signe='verseau'}
+scrap(signe)
+}//fin fnct
+
+function scrap(signe){
+console.log(signe); request = require('request'); cheerio = require('cheerio');
+var url = 'http://www.horoscope-gratuit.org/horoscope-'+signe+'.php';
+request({ 'uri' : url , 'headers':{'Accept-Charset': 'utf-8'}, 'encoding':'binary' }, function (err, response, body){
+$ = require('cheerio').load(body, { xmlMode: true, ignoreWhitespace: false, lowerCaseTags: false });
+a= $('div:nth-child(1)').text();
+b=''
+for(i=a.search(':')+2;i<a.length;i++){b=b+a[i]} 
+ScribeSpeak('pour les '+signe+' ; '+b,true) 
+//SARAH.run('mathildedico', { 'phrase' : b})		
+callback({'tts' : " "});
+return false
+})//fin request
+}//fin fnct scrap
+signes(phrasehoroscope,signe)
+} 
